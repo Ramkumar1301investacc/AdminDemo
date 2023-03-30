6e6c5cc7e68c351d88fb8d8a2f5972a647f4b1bb
Ramkumar1301investacc <118898628+Ramkumar1301investacc@users.noreply.github.com>
2023-03-30 17:53:20 +0530

    added mkixpanel

--- giliadmin/src/app/component/policy/policy.component.ts
+++ giliadmin/src/app/component/policy/policy.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./policy.component.css']
 })
 export class PolicyComponent {
-
+  
+ 
+  
+  
+  
 }
--- giliadmin/src/app/component/sidenavbar/sidenavbar.component.ts
+++ giliadmin/src/app/component/sidenavbar/sidenavbar.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import mixpanel from 'mixpanel-browser';
+
+
+
 @Component({
   selector: 'app-sidenavbar',
   templateUrl: './sidenavbar.component.html',
   styleUrls: ['./sidenavbar.component.css']
 })
-export class SidenavbarComponent {
 
+export class SidenavbarComponent {
+  ngOnInit() {
+    mixpanel.init("11b25c9684cd81a697eb5776a555af3e");
+  }
+  
+  onStoryboardClick() {
+    mixpanel.track("Storyboard Clicked");
+    console.log('storyboardclicked')
+  }
 }
